ea67792cf040864b5c13528299fd76c1d80f747f
YashrajPawar <yashrajpawar33@gmail.com>
2023-04-14 15:26:26 +0530

    made the link secure

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://YashrajPawar:Renukaapril01@cluster0.mjamzjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    url: 'mongodb+srv://YashrajPawar:<password>@cluster0.mjamzjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 }
